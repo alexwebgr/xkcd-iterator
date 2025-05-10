05f1aa4c917cfd9ed4f6ea0530ed721fa1564367
alexwebgr <websurfer992@gmail.com>
2025-05-10 17:06:55 +0300

    ui updates

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -4,10 +4,12 @@ class Comic < ApplicationRecord
   validates :safe_title, presence: true
   validates :img_url, presence: true
 
-  scope :by_num, ->(comic_num) { find_by_num(comic_num) }
-
   def self.last_num
     comic = Comic.order(num: :desc).first
     comic.present? ? comic.num : 1
+  end
+
+  def self.by_num(comic_num)
+    find_by_num(comic_num)
   end
 end
